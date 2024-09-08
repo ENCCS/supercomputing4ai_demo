dcfade6e285570e5fe8dff4b16a754be5bd57a87
Francesco Fiusco <ffiusco94@gmail.com>
2024-09-08 19:08:51 +0200

    Initial commit

---
+++ models/serving/main.py
@@ -0,0 +1,135 @@
+import os
+import numpy as np
+from tqdm import tqdm
+import cv2 as cv
+from PIL import Image
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import sys
+
+from keras import Model
+from keras.callbacks import Callback
+from keras.optimizers import Adam
+from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Dropout
+
+####################################################
+# Define U-net architecture
+####################################################
+
+SIZE = 128
+
+def unet_model(input_layer, start_neurons):
+    # Contraction path
+    conv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(input_layer)
+    conv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(0.25)(pool1)
+
+    conv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(0.5)(pool2)
+
+    conv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(0.5)(pool3)
+
+    conv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(0.5)(pool4)
+
+    # Middle
+    convm = Conv2D(start_neurons*16, kernel_size=(3, 3), activation="relu", padding="same")(pool4)
+    convm = Conv2D(start_neurons*16, kernel_size=(3, 3), activation="relu", padding="same")(convm)
+    
+    # Expansive path
+    deconv4 = Conv2DTranspose(start_neurons*8, kernel_size=(3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(0.5)(uconv4)
+    uconv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(uconv4)
+    uconv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(uconv4)
+
+    deconv3 = Conv2DTranspose(start_neurons*4, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(0.5)(uconv3)
+    uconv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(uconv3)
+    uconv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(uconv3)
+
+    deconv2 = Conv2DTranspose(start_neurons*2, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+    uconv2 = Dropout(0.5)(uconv2)
+    uconv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(uconv2)
+    uconv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(uconv2)
+
+    deconv1 = Conv2DTranspose(start_neurons*1, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+    uconv1 = Dropout(0.5)(uconv1)
+    uconv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(uconv1)
+    uconv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(uconv1)
+    
+    # Last conv and output
+    output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    
+    return output_layer
+
+# Compile unet model
+input_layer = Input((SIZE, SIZE, 3))
+output_layer = unet_model(input_layer = input_layer, start_neurons = 16)
+
+model = Model(input_layer, output_layer)
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.summary()
+
+model_name = "unet2"
+weights_path = '../unet/result/models/' + model_name 
+model.load_weights(weights_path)
+print("model: ", weights_path)
+
+image_name = sys.argv[1]
+image_path = '../images/' + image_name
+print("image: ", image_path)
+
+image = Image.open(image_path)
+
+# Preprocess the image
+image = image.resize((SIZE, SIZE))
+image_array = np.asarray(image).astype('float32') / 255.0  # Normalize pixel values if your model expects this
+image_array = np.expand_dims(image_array, axis=0)
+
+# Perform inference
+prediction = model.predict(image_array)
+
+if prediction.shape[-1] == 1:
+    predicted_mask = prediction[0, :, :, 0]  # Remove batch dimension and get the mask
+    thresholded_mask = (predicted_mask > 0.5).astype(np.uint8)  # Apply a threshold to get a binary mask
+
+original_image = Image.open(image_path)
+original_image = original_image.resize((SIZE, SIZE))
+original_image_array = np.asarray(original_image).astype('float32') / 255.0
+
+image_for_model = np.expand_dims(original_image_array, axis=0)  # Add batch dimension
+predicted_mask = model.predict(image_for_model)[0, :, :, 0]  # Assuming the model outputs a single-channel mask
+thresholded_mask = (predicted_mask > 0.5).astype(np.uint8)
+red_mask = np.zeros_like(original_image_array)
+red_mask[:, :, 0] = 1  # Set the red channel to maximum
+colored_image = np.where(thresholded_mask[:, :, np.newaxis] == 1, red_mask, original_image_array)
+
+dpi = 600  # Change the dpi if needed
+figsize = (image.size[0]+100 / dpi, image.size[1]+100 / dpi)
+
+plt.figure(figsize=figsize)  # Adjust figure size as needed
+plt.imshow(colored_image)
+plt.tight_layout(pad=0)
+plt.axis('off')  # Hide the axis
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+plt.margins(0, 0)
+plt.gca().xaxis.set_major_locator(plt.NullLocator())
+plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+image_path = '../generated-images/' + image_name
+
+if not os.path.exists('..' + '/generated-images/'):
+    os.makedirs('..' + '/generated_images')
+plt.savefig(image_path)
---
+++ models/unet/main.py
@@ -0,0 +1,204 @@
+import os
+import numpy as np
+from tqdm import tqdm
+import cv2 as cv
+from PIL import Image
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import sys
+
+from keras import Model
+from keras.callbacks import Callback
+from keras.optimizers import Adam
+from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Dropout
+
+projdir = '.'
+
+if not os.path.exists(projdir + '/result/training/'):
+    os.makedirs(projdir + '/result/training/')
+
+if not os.path.exists(projdir + '/result/models/'):
+    os.makedirs(projdir + '/result/models/')
+
+####################################################
+# Data preparation
+####################################################
+
+image_path = projdir + '/data/water/Images/'
+mask_path = projdir + '/data/water/Masks/'
+
+SIZE = 128
+
+# Lists of images and masks names
+image_names = sorted(next(os.walk(image_path))[-1])
+mask_names = sorted(next(os.walk(mask_path))[-1])
+
+images = np.zeros(shape=(len(image_names),SIZE, SIZE, 3))
+masks = np.zeros(shape=(len(image_names),SIZE, SIZE, 1))
+
+for id in tqdm(range(len(image_names)), desc="Images"):
+  path = image_path + image_names[id]
+  img = np.asarray(Image.open(path)).astype('float')/255.
+  img = cv.resize(img, (SIZE,SIZE), cv.INTER_AREA)
+  images[id] = img
+
+for id in tqdm(range(len(mask_names)), desc="Mask"):
+  path = mask_path + mask_names[id]
+  mask = np.asarray(Image.open(path)).astype('float')/255.
+  mask = cv.resize(mask, (SIZE,SIZE), cv.INTER_AREA)
+  masks[id] = mask[:,:,:1]
+
+# Train test split
+images_train, images_test, mask_train, mask_test = train_test_split(images, masks, test_size=0.25)
+
+####################################################
+# Define U-net architecture
+####################################################
+
+def unet_model(input_layer, start_neurons):
+    # Contraction path
+    conv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(input_layer)
+    conv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(0.25)(pool1)
+
+    conv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(0.5)(pool2)
+
+    conv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(0.5)(pool3)
+
+    conv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(0.5)(pool4)
+
+    # Middle
+    convm = Conv2D(start_neurons*16, kernel_size=(3, 3), activation="relu", padding="same")(pool4)
+    convm = Conv2D(start_neurons*16, kernel_size=(3, 3), activation="relu", padding="same")(convm)
+    
+    # Expansive path
+    deconv4 = Conv2DTranspose(start_neurons*8, kernel_size=(3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(0.5)(uconv4)
+    uconv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(uconv4)
+    uconv4 = Conv2D(start_neurons*8, kernel_size=(3, 3), activation="relu", padding="same")(uconv4)
+
+    deconv3 = Conv2DTranspose(start_neurons*4, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(0.5)(uconv3)
+    uconv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(uconv3)
+    uconv3 = Conv2D(start_neurons*4, kernel_size=(3, 3), activation="relu", padding="same")(uconv3)
+
+    deconv2 = Conv2DTranspose(start_neurons*2, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+    uconv2 = Dropout(0.5)(uconv2)
+    uconv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(uconv2)
+    uconv2 = Conv2D(start_neurons*2, kernel_size=(3, 3), activation="relu", padding="same")(uconv2)
+
+    deconv1 = Conv2DTranspose(start_neurons*1, kernel_size=(3, 3), strides=(2, 2), padding="same")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+    uconv1 = Dropout(0.5)(uconv1)
+    uconv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(uconv1)
+    uconv1 = Conv2D(start_neurons, kernel_size=(3, 3), activation="relu", padding="same")(uconv1)
+    
+    # Last conv and output
+    output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    
+    return output_layer
+
+# Compile unet model
+input_layer = Input((SIZE, SIZE, 3))
+output_layer = unet_model(input_layer = input_layer, start_neurons = 16)
+
+model = Model(input_layer, output_layer)
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.summary()
+
+sys.stdout.flush()
+
+####################################################
+# Training
+#####################################################
+
+def mask_threshold(image, threshold=0.25):
+  return image>threshold
+
+epochs = 100 
+batch_size = 32
+
+history = model.fit(images_train, mask_train,
+                    validation_data=[images_test, mask_test], 
+                    epochs=epochs,
+                    batch_size=batch_size)
+
+plt.figure(figsize=(10,5))
+
+# Plot loss for each epoch
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], label="Train loss")
+plt.plot(history.history['val_loss'], label="Test loss")
+plt.title('Binary crossentropy loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['TRAIN', 'TEST'], loc='upper left')
+
+# Plot accuracy for each epoch
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], label="Train accuracy")
+plt.plot(history.history['val_accuracy'], label="Test accuracy")
+plt.title('Accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['TRAIN', 'TEST'], loc='upper left')
+
+plt.tight_layout()
+
+
+plt.savefig(projdir + '/result/training/res_' + processid + '.png')
+
+####################################################
+# Save model weights 
+####################################################
+
+name = "unet2" 
+weights_path = os.path.join(projdir, 'result', 'models', f'weights_{name}.h5')
+model.save_weights(weights_path)
+print(f"Model weights saved to {weights_path}")
+
+####################################################
+# Make predictions
+####################################################
+
+predictions = model.predict(images_test)
+
+def plot_results(threshold=0.5):
+    k = np.random.randint(len(images_test))
+    original_image = images_test[k]
+    mask = mask_test[k].reshape(128,128)
+    predicted_mask = predictions[k].reshape(128,128)
+    threshold_mask = mask_threshold(predicted_mask, threshold=threshold)
+
+    plt.figure(figsize=(15,5))
+
+    plt.subplot(1,4,1)
+    plt.imshow(original_image);plt.title('Orginal Image')
+
+    plt.subplot(1,4,2)
+    plt.imshow(mask, cmap='gray');plt.title('Original Mask')
+
+    plt.subplot(1,4,3)
+    plt.imshow(predicted_mask, cmap='gray');plt.title('Predicted Mask')
+    
+    plt.subplot(1,4,4)
+    plt.imshow(threshold_mask, cmap='gray');plt.title(f'Predicted Mask with cutoff={threshold}')
+
+    plt.tight_layout()
+    plt.savefig(projdir + '/result/training/samples_' + processid + '.png')
+
+# Plot results on test data
+plot_results(threshold=0.4)
